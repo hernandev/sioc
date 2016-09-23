da7bd15ce5ae5b89a4eee32b6efd258394ba46c1
Diego Hernandes <diego@hernandev.com>
2016-09-23 19:59:41 -0300

    Container singleton skeleton

---
+++ src/Container.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sioc;
+
+/**
+ * Class Container.
+ */
+class Container
+{
+    /**
+     * @var Container Instance of Sioc container
+     */
+    protected static $instance;
+
+    /**
+     * @var array List of IoC Bindings
+     */
+    protected $bindings = [];
+
+    /**
+     * @return Container Current Sioc container instance
+     */
+    public static function getInstance()
+    {
+        // if there is not a instance yet, create a new one
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+
+        // return the new or already existing instance
+        return self::$instance;
+    }
+
+    /**
+     * Container constructor.
+     *
+     * Constructor is protected so people can never
+     * do "new Container()"
+     */
+    protected function __construct()
+    {
+        //
+    }
+}
