483fcb975df9baa464924ca60b1462c1075e84a2
Diego Hernandes <diego@hernandev.com>
2016-09-23 21:20:30 -0300

    some commenting

--- start.php
+++ start.php
@@ -2,17 +2,25 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+// container instance
 $container = \Sioc\Container::getInstance();
 
+// create a database instance
 $database = new \Demo\Database();
 
+// register database as singleton that will resolve
+// always the same instance
 $container->register(\Demo\DatabaseInterface::class, new \Demo\Database());
 
+// register \Demo\User on the alias user
 $container->register('user', \Demo\User::class);
 
-/** @var \Demo\User $user */
+// Resolve the 'user'
 $user = $container->make('user');
 
+// show the user instance
 var_dump($user);
+// verify it's working
 var_dump($user->getName());
+// verify the database instance is the same on the singleton
 var_dump($user->getDatabase() == $database);
